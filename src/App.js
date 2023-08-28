6f7a16937da680c56911564b0f035f4681666668
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-28 20:14:13 +0800

    feat: create initial todo item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import TodoItem from "./components/TodoItem";
 
 function App() {
     return (
         <div className="App">
             <h1>Todo List</h1>
+            <TodoItem />
         </div>
     );
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,6 @@
+
+const TodoItem = () => {
+    return <div>This is the first Todo Item</div>;
+};
+
+export default TodoItem;
