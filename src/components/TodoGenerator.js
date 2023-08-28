9f4f1d9be47b428af5d7d10715effc11d0243a15
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-28 22:10:06 +0800

    feat: handle null inputs

--- src/components/TodoGenerator.js
+++ src/components/TodoGenerator.js
@@ -8,12 +8,19 @@ const TodoGenerator = (props) => {
     };
 
     const addItem = () => {
-        props.onAddItem(item);
+        if (!item.trim() == null) {
+            props.onAddItem(item);
+        }
     };
 
     return (
         <div>
-            <input type="text" onChange={onItemChange} className="item-input" placeholder="What are you going to do today?"/>
+            <input
+                type="text"
+                onChange={onItemChange}
+                className="item-input"
+                placeholder="What are you going to do today?"
+            />
             <button onClick={addItem} className="add-button">
                 Add
             </button>
