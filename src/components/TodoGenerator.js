3268df28cb2452e019efb2b7296f1a564caf64f4
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-28 21:02:29 +0800

    feat: creat add function to todo list

--- src/components/TodoGenerator.js
+++ src/components/TodoGenerator.js
@@ -1,8 +1,20 @@
-const TodoGenerator = () => {
+import { useState } from "react";
+
+const TodoGenerator = (props) => {
+    const [item, setItem] = useState("");
+
+    const onItemChange = (event) => {
+        setItem(event.target.value);
+    };
+
+    const addItem = () => {
+        props.onAddItem(item);
+    };
+
     return (
         <div>
-            <input type="text" />
-            <button>Add</button>
+            <input type="text" onChange={onItemChange} />
+            <button onClick={addItem}>Add</button>
         </div>
     );
 };
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import TodoGenerator from "./TodoGenerator";
 import TodoGroup from "./TodoGroup";
 
 const TodoList = () => {
+    const [todoItems, setTodoItems] = useState([]);
+
+    const onAddItem = (item) => {
+        setTodoItems(todoItems.concat(item));
+    };
+
     return (
         <div className="App">
             <h1>Todo List</h1>
-            <TodoGroup todoItems={["First", "Second"]} />
-            <TodoGenerator />
+            <TodoGroup todoItems={todoItems} />
+            <TodoGenerator onAddItem={onAddItem} />
         </div>
     );
 };
