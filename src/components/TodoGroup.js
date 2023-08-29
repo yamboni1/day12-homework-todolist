28bddfbd720c8d27b7306317b640af41686ee4f6
yamboni1 <ncyambot.work@gmail.com>
2023-08-29 21:13:11 +0800

    refactor: added a div for the todoItems and added the css

--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -3,9 +3,17 @@ import TodoItem from "./TodoItem";
 
 const TodoGroup = () => {
     const todoItems = useSelector((state) => state.todoList.todoList);
-    return todoItems.map((todoItem, index) => {
-        return <TodoItem todoItem={todoItem} key={todoItem.id} index={index} />;
-    });
-};
+    return (
+    <div className="todoGroup">
+        {
+             todoItems.map((todoItem, index) => {
+                return <TodoItem todoItem={todoItem} key={todoItem.id} index={index} />
+            })
+        }   
+
+    </div>
+    )
+    
+}
 
 export default TodoGroup;
