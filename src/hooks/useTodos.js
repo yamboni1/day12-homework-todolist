e85f68cce0ff35e72c057faf62cbe8fc50719396
yamboni1 <ncyambot.work@gmail.com>
2023-08-31 01:08:33 +0800

    feat: implement the use of hooks

--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -4,11 +4,37 @@ import { resetTodoTask } from "../components/todoListSlice";
 
 export const useTodos = () => {
     const dispatch = useDispatch();
-         async function loadTodos() {
-            const response = await todoApi.getTodoTasks();
-            dispatch(resetTodoTask(response.data));
-        }return{
-            loadTodos
-        }
+    async function loadTodos() {
+        const response = await todoApi.getTodoTasks();
+        dispatch(resetTodoTask(response.data));
+    }
+    async function toggleItem(id,todoItem) {
+        await todoApi.updateTodoTask(todoItem.id, {
+            done: !todoItem.done,
+        });
+        loadTodos();
+    }
+    async function deleteItem(id){
+        await todoApi.deleteTodoTask(id);
+        loadTodos();
+    }
+    async function addItem(todoItem){
+        await todoApi.addTodoTask(todoItem);
+        loadTodos();
+    }
+    async function updateItem(id,textToUpdate) {
+        await todoApi.updateTodoTask(id, {
+            text: textToUpdate
+        });
+        loadTodos();
+    }
+    
+    return {
+        loadTodos,
+        toggleItem,
+        deleteItem,
+        addItem,
+        updateItem
+    }
 
 }
