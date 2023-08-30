6335b36fc896b814726efdc633e1ff1ad6082c55
yamboni1 <ncyambot.work@gmail.com>
2023-08-30 21:37:26 +0800

    feat: update the implementation to use hooks

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,11 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import * as todoApi from "./../apis/todoApi";
 import TodoGenerator from "./TodoGenerator";
 import TodoGroup from "./TodoGroup";
-import { resetTodoTask } from "./todoListSlice";
+import { useTodos } from "../hooks/useTodos";
 
 const TodoList = (props) => {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        async function fetchData() {
-            const response = await todoApi.getTodoTasks();
-            dispatch(resetTodoTask(response.data));
-        }
-        fetchData();
-    },[]);
+        const{loadTodos} = useTodos();
+        useEffect(()=> {loadTodos();},[])
     return (
         <>
             <h1>Todo List</h1>
